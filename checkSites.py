ed0479e8c3992a153688f6e536ff83f61458a1bb
Dimitris Bachtis <bachtisd@gmail.com>
2014-11-19 14:41:10 +0200

    Update checkSites.py

--- checkSites.py
+++ checkSites.py
@@ -1,16 +1,22 @@
+#!/usr/bin/python
+
 import httplib
 import csv
  
 def csv_reader(file_obj):
-    """
-    Read a csv file
-    """
+    online=0
+    total=0
     reader = csv.reader(file_obj)
     for row in reader:
+        total=total+1
         conn = httplib.HTTPConnection(row)
         conn.request("HEAD", "/")
         r1 = conn.getresponse()
+        if r1.status == 200:
+            online=online+1
         print row, r1.status, r1.reason
+    
+    print online+"/"+total
  
 if __name__ == "__main__":
     csv_path = "sites.csv"
